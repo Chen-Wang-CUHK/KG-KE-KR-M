03cefcb49b2f482067bac7465ede8b576bb68424
Chen-Wang-CUHK <wangzaicuhk@gmail.com>
2019-06-25 21:17:41 +0800

    fix a bug in onmt/models/__init__.py

--- KG-KE-KR/onmt/models/__init__.py
+++ KG-KE-KR/onmt/models/__init__.py
@@ -1,4 +1,6 @@
 """Module defining models."""
+from onmt.models.model_saver import build_model_saver, ModelSaver
+from onmt.models.model import NMTModel, SelectorModel, E2EModel
 
 __all__ = ["build_model_saver", "ModelSaver",
            "NMTModel", "E2EModel", "SelectorModel", "ReRankerModel", "check_sru_requirement"]
